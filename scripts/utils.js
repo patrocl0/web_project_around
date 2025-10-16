d5f6eda29312d8151df2e04d6fb2825b7ff09216
Patroclo <stivencardenas1208@gmail.com>
2025-10-16 05:36:37 +0000

    Se refactoriza el javascript(index.js) en diferentes clases para que el codigo sea mas legible, ademas se hizo una mejora en las tarjetas y el funcionamiento de ellas

---
+++ scripts/utils.js
@@ -0,0 +1,42 @@
+const modal = document.querySelector(".modal--normal");
+const modalBody = document.querySelector("#modal-body");
+const overlay = document.querySelector(".overlay");
+const closeModalBtn = document.querySelector("#btn-close");
+
+let activeModal = null;
+
+export function openModal(templateContent, size) {
+  modalBody.replaceChildren(templateContent.cloneNode(true));
+  modal.classList.remove("modal--normal", "modal--large", "hidden");
+  modal.classList.add(`modal--${size}`);
+  overlay.classList.remove("hidden");
+
+  activeModal = modal;
+
+  document.addEventListener("keydown", handleEscape);
+  overlay.addEventListener("click", handleOverlayClick);
+}
+
+export function closeModal() {
+  const overlay = document.querySelector(".overlay");
+
+  if (!activeModal) return;
+
+  activeModal.classList.add("hidden");
+  overlay.classList.add("hidden");
+
+  activeModal = null;
+
+  document.removeEventListener("keydown", handleEscape);
+  overlay.removeEventListener("click", handleOverlayClick);
+}
+
+function handleEscape(evt) {
+  if (evt.key === "Escape" && activeModal) {
+    closeModal();
+  }
+}
+
+function handleOverlayClick() {
+  closeModal();
+}
